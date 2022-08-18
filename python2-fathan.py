94dc7387f972c29ac0d6e6cfaf25d2fc221c6ab1
ptnrfif07 <111480669+ptnrfif07@users.noreply.github.com>
2022-08-18 13:56:51 +0700

    Add files via upload

---
+++ python2-fathan.py
@@ -0,0 +1,8 @@
+nama_depan="Fathan"
+print("nama depan anda adalah",nama_depan)
+
+nama_belakang="Athalah"
+print("nama belakang anda adalah",nama_belakang)
+
+nama_lengkap=nama_depan,nama_belakang
+print("nama lengkap anda adalah",nama_depan,nama_belakang)
