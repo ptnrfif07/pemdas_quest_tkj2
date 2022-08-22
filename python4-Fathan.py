3f925aec56f3cc9944e8df53b29edf61659b6c44
ptnrfif07 <111480669+ptnrfif07@users.noreply.github.com>
2022-08-22 13:34:46 +0700

    Add files via upload

---
+++ python4-Fathan.py
@@ -0,0 +1,16 @@
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_sg = 1/2 * alas * tinggi
+
+print("luas segitiga adalah: ", luas_sg)
+
+s = int(input("masukan nilai sisi: "))
+luas_persegi = s * s
+
+print("luas persegi adalah:", luas_persegi)
+
+p = int(input("masukan nilai panjang "))
+l = int(input("masukan nilai panjang "))
+luas_pj = p * l
+
+print("luas persegi panjang adalah:", luas_pj)
