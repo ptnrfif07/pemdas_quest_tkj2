493ead279af588d0d0b4a212aeed42627528e59e
ptnrfif07 <111480669+ptnrfif07@users.noreply.github.com>
2022-08-22 11:58:33 +0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,15 @@
+s = input ("Nama depan anda: ")
+h = input ("Nama belakang anda: ")
+print("Nama anda adalah",s,h)
+
+a =int(input("masukan nilai a: "))
+b =float(input("masukan nilai b "))
+c = a+b
+
+print("A=", a)
+print("B+", b)
+print(a,"+",b,"=",c)
+
+x = int(input("masukan suhu celcius: "))
+y = (9/5 * x) + 32
+print(x,"celsius =",y,"fahrenhait")
